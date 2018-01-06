7e6f4b7e970a04c534df49dc305bbd5bd3489183
JesusDF <jesusdf@gmail.com>
2018-01-06 02:34:45 +0100

    Add a script to show basic system information.

---
+++ i386/clientupdate_1.0-1/usr/local/bin/sysinfo.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+SYSINFO=`head -n 1 /etc/issue | cut -d\\\\ -f 1`
+IFS=$'\n'
+UPTIME=`uptime`
+D_UP=${UPTIME:1}
+MYGROUPS=`groups`
+DATE=`date`
+KERNEL=`uname -a`
+CPWD=`pwd`
+ME=`whoami`
+CPU=`arch`
+MODES=`LANG=C lscpu | grep op-mode | head -n 1 | cut -d: -f2 | xargs`
+SOCKETS=`LANG=C lscpu | grep Socket | head -n 1 | cut -d: -f2 | xargs`
+NAME=`LANG=C lscpu | grep name | head -n 1 | cut -d: -f2 | xargs`
+CORES=`LANG=C lscpu | grep Core\(s\) | head -n 1 | cut -d: -f2 | xargs`
+THREADS=`LANG=C lscpu | grep CPU\(s\) | grep -v NUMA | grep -v list | head -n 1 | cut -d: -f2 | xargs`
+GPU=`lspci | grep VGA | cut -d: -f 3 | xargs`
+printf "<=== SYSTEM ===>\n"
+echo "  Distribution:	"$SYSINFO""
+printf "  Kernel:\t"$KERNEL"\n"
+printf "  Uptime:\t"$D_UP"\n"
+printf "  Date:\t\t"$DATE"\n"
+free -mt | awk '
+/Mem/{print "  Memory:\tTotal: " $2 "Mb\tUsed: " $3 "Mb\tFree: " $4 "Mb"}
+/Swap/{print "  Swap:\t\tTotal: " $2 "Mb\tUsed: " $3 "Mb\tFree: " $4 "Mb"}'
+printf "  Linux Arch:\t"$CPU"\n"
+#cat /proc/cpuinfo | grep "model name\|processor" | awk '
+#/processor/{printf "  Processor:\t" $3 " : " }
+#/model\ name/{
+#i=4
+#while(i<=NF){
+#	printf $i
+#	if(i<NF){
+#		printf " "
+#	}
+#	i++
+#}
+#printf "\n"
+#}'
+printf "  CPU Name:\t$NAME\n"
+printf "  CPU Arch:\t$MODES\n"
+printf "  CPU Sockets:\t$SOCKETS\n"
+printf "  CPU Cores:\t$CORES\n"
+printf "  CPU Threads:\t$THREADS\n"
+printf "  GPU Name:\t$GPU\n"
+printf "\n<=== USER ===>\n"
+printf "  User:\t\t"$ME" (uid:"$UID")\n"
+printf "  Groups:\t"$MYGROUPS"\n"
+printf "  Working dir:\t"$CPWD"\n"
+printf "  Home dir:\t"$HOME"\n"
+printf "\n<=== NETWORK ===>\n"
+printf "  Hostname:\t"$HOSTNAME"\n"
+ip -o addr | awk '/inet /{print "  IP (" $2 "):\t" $4}'
+/sbin/route -n | awk '/^0.0.0.0/{ printf "  Gateway:\t"$2"\n" }'
+cat /etc/resolv.conf | awk '/^nameserver/{ printf "  Name Server:\t" $2 "\n"}'
