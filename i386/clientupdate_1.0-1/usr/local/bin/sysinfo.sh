37fa00a2850ca1a326ec5911de92711842790a27
JesusDF <jesusdf@gmail.com>
2018-01-06 02:53:55 +0100

    Add disk information to sysinfo.sh

--- i386/clientupdate_1.0-1/usr/local/bin/sysinfo.sh
+++ i386/clientupdate_1.0-1/usr/local/bin/sysinfo.sh
@@ -18,13 +18,13 @@ THREADS=`LANG=C lscpu | grep CPU\(s\) | grep -v NUMA | grep -v list | head -n 1 
 GPU=`lspci | grep VGA | cut -d: -f 3 | xargs`
 printf "<=== SYSTEM ===>\n"
 echo "  Distribution:	"$SYSINFO""
+printf "  Linux Arch:\t"$CPU"\n"
 printf "  Kernel:\t"$KERNEL"\n"
 printf "  Uptime:\t"$D_UP"\n"
 printf "  Date:\t\t"$DATE"\n"
 free -mt | awk '
 /Mem/{print "  Memory:\tTotal: " $2 "Mb\tUsed: " $3 "Mb\tFree: " $4 "Mb"}
 /Swap/{print "  Swap:\t\tTotal: " $2 "Mb\tUsed: " $3 "Mb\tFree: " $4 "Mb"}'
-printf "  Linux Arch:\t"$CPU"\n"
 #cat /proc/cpuinfo | grep "model name\|processor" | awk '
 #/processor/{printf "  Processor:\t" $3 " : " }
 #/model\ name/{
@@ -38,19 +38,23 @@ printf "  Linux Arch:\t"$CPU"\n"
 #}
 #printf "\n"
 #}'
+printf "\n<=== CPU ===>\n"
 printf "  CPU Name:\t$NAME\n"
 printf "  CPU Arch:\t$MODES\n"
 printf "  CPU Sockets:\t$SOCKETS\n"
 printf "  CPU Cores:\t$CORES\n"
 printf "  CPU Threads:\t$THREADS\n"
+printf "\n<=== GPU ===>\n"
 printf "  GPU Name:\t$GPU\n"
-printf "\n<=== USER ===>\n"
-printf "  User:\t\t"$ME" (uid:"$UID")\n"
-printf "  Groups:\t"$MYGROUPS"\n"
-printf "  Working dir:\t"$CPWD"\n"
-printf "  Home dir:\t"$HOME"\n"
+printf "\n<=== DISK ===>\n"
+lsscsi | cut -d\  -f3-
 printf "\n<=== NETWORK ===>\n"
 printf "  Hostname:\t"$HOSTNAME"\n"
 ip -o addr | awk '/inet /{print "  IP (" $2 "):\t" $4}'
 /sbin/route -n | awk '/^0.0.0.0/{ printf "  Gateway:\t"$2"\n" }'
 cat /etc/resolv.conf | awk '/^nameserver/{ printf "  Name Server:\t" $2 "\n"}'
+printf "\n<=== USER ===>\n"
+printf "  User:\t\t"$ME" (uid:"$UID")\n"
+printf "  Groups:\t"$MYGROUPS"\n"
+printf "  Working dir:\t"$CPWD"\n"
+printf "  Home dir:\t"$HOME"\n"
