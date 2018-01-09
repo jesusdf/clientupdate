39abc69bbc3bfe76d9da6b632a18b8cc5ffdcd26
JesusDF <jesusdf@gmail.com>
2018-01-10 00:48:50 +0100

    MKV2MP4 script should first try GPU encode with decent quality, if it fails should use CPU with fast encoding and if it also fails, then just video copy it.

--- i386/clientupdate_1.0-1/usr/local/bin/mkvtrans.sh
+++ i386/clientupdate_1.0-1/usr/local/bin/mkvtrans.sh
@@ -2,15 +2,14 @@
 DIR=$(dirname "$1")
 FILE=$(basename "$1")
 SUBS=mysubs.${1##*.}
-NVIDIA_GPU=$(lspci | grep VGA | grep NVIDIA | wc -l)
 THREADS=$(getconf _NPROCESSORS_ONLN)
 
 cd $DIR
-if [ "${NVIDIA_GPU}" -eq "0" ]; then
-    ffmpeg -threads $THREADS -i "$FILE" -stats -vcodec copy -acodec copy "${1%.*}.mp4"
-else
-    ln -s "$FILE" $SUBS
-    ffmpeg -threads $THREADS -i "$FILE" -vf subtitles=$SUBS -stats -c:v h264_nvenc -level 4.0 -rc cbr -acodec copy "${1%.*}.mp4"
+if [ -f $SUBS ]; then
     rm -f $SUBS
 fi
+ln -s "$FILE" $SUBS
+ffmpeg -threads $THREADS -i "$FILE" -vf "subtitles=$SUBS,scale=1280:720" -stats -c:v h264_nvenc -r ntsc -profile:v high -level 4.0 -rc cbr -acodec copy "${1%.*}.mp4" || ( rm -f "${1%.*}.mp4" && ffmpeg -threads $THREADS -i "$FILE" -vf subtitles=$SUBS -stats -c:v libx264 -r ntsc -profile:v high -preset superfast -level 4.0 -rc cbr -acodec copy "${1%.*}.mp4" ) || ( rm -f "${1%.*}.mp4" && ffmpeg -threads $THREADS -i "$FILE" -stats -vcodec copy -acodec copy "${1%.*}.mp4" )
+rm -f $SUBS
+
 DISPLAY=:0 /usr/bin/notify-send -i dialog-information -u normal "$FILE convertido a MP4.";
