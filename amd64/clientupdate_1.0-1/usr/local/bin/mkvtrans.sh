227fda378045b8e6ce2c9a8657d45fd055c2a63c
JesusDF <jesusdf@gmail.com>
2018-01-15 00:39:58 +0100

    Adjust transcoding parameters and desktop files. Add a new desktop file to transcode a single file.

--- amd64/clientupdate_1.0-1/usr/local/bin/mkvtrans.sh
+++ amd64/clientupdate_1.0-1/usr/local/bin/mkvtrans.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-DIR=$(dirname "$1")
-FILE=$(basename "$1")
-SUBS=mysubs.${1##*.}
+PARAMS=$@
+DIR=$(dirname "$PARAMS")
+FILE=$(basename "$PARAMS")
+SUBS=mysubs$RANDOM.${PARAMS##*.}
 THREADS=$(getconf _NPROCESSORS_ONLN)
 
 cd $DIR
@@ -9,7 +10,7 @@ if [ -f $SUBS ]; then
     rm -f $SUBS
 fi
 ln -s "$FILE" $SUBS
-ffmpeg -threads $THREADS -i "$FILE" -vf "subtitles=$SUBS,scale=1280:720" -stats -c:v h264_nvenc -r ntsc -profile:v high -level 4.0 -rc cbr -acodec copy "${1%.*}.mp4" || ( rm -f "${1%.*}.mp4" && ffmpeg -threads $THREADS -i "$FILE" -vf subtitles=$SUBS -stats -c:v libx264 -r ntsc -profile:v high -preset superfast -level 4.0 -rc cbr -acodec copy "${1%.*}.mp4" ) || ( rm -f "${1%.*}.mp4" && ffmpeg -threads $THREADS -i "$FILE" -stats -vcodec copy -acodec copy "${1%.*}.mp4" )
+ffmpeg -threads $THREADS -i "$FILE" -vf "subtitles=$SUBS,scale=1280:720" -stats -c:v h264_nvenc -r ntsc -profile:v high -level 4.0 -rc cbr -acodec copy "${PARAMS%.*}.mp4" || ( rm -f "${PARAMS%.*}.mp4" && ffmpeg -threads $THREADS -i "$FILE" -vf subtitles=$SUBS -stats -c:v libx264 -r ntsc -profile:v high -preset faster -level 4.0 -rc cbr -acodec copy "${PARAMS%.*}.mp4" ) || ( rm -f "${PARAMS%.*}.mp4" && ffmpeg -threads $THREADS -i "$FILE" -stats -vcodec copy -acodec copy "${PARAMS%.*}.mp4" )
 rm -f $SUBS
 
 DISPLAY=:0 /usr/bin/notify-send -i dialog-information -u normal "$FILE convertido a MP4.";
