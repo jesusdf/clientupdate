15e8c7d55238359e42064f26a2328332da620bff
JesusDF <jesusdf@gmail.com>
2018-01-15 00:53:44 +0100

    Compare the motd file, not linuxlogo output.

--- amd64/clientupdate_1.0-1/etc/init.d/clientupdate.sh
+++ amd64/clientupdate_1.0-1/etc/init.d/clientupdate.sh
@@ -151,7 +151,7 @@ config)
                         sync;
                 fi;
         # nvidiadriver y linuxlogo
-        if [ "$(/usr/bin/linuxlogo | grep "$(uname -v)" | wc -l)" -eq "0" ]; then
+        if [ "$(cat /etc/motd | grep "$(uname -v)" | wc -l)" -eq "0" ]; then
             /etc/kernel/postinst.d/nvidiadriver;
             /usr/local/bin/setscheduler.sh performance;
             /usr/bin/linuxlogo > /etc/motd;
