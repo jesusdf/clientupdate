db90d340bf7a6b50de60d9364c906f6cea057768
JesusDF <jesusdf@gmail.com>
2018-02-11 22:43:59 +0100

    Linuxlogo kernel postinstall script should not cause any trouble if it's not installed. Fix bug in aptkey.sh script.

--- amd64/clientupdate_1.0-1/usr/local/bin/aptkey.sh
+++ amd64/clientupdate_1.0-1/usr/local/bin/aptkey.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-gpg --keyserver hkp://keyserver.ubuntu.com --recv-keys "$1" | gpg --keyserver hkp://pgp.mit.edu --recv-keys "$1" | gpg --keyserver hkp://subkeys.pgp.net --recv-keys "$1"
+gpg --keyserver hkp://keyserver.ubuntu.com --recv-keys "$1" || gpg --keyserver hkp://pgp.mit.edu --recv-keys "$1" || gpg --keyserver hkp://subkeys.pgp.net --recv-keys "$1"
 gpg --export --armor "$1" | apt-key add -
