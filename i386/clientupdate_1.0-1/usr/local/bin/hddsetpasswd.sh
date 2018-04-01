57dc6e1f98b2bc4c56d14539810286d5bbb2426b
JesusDF <jesusdf@gmail.com>
2018-04-01 23:32:22 +0200

    Add another command to set the system asleep.

--- i386/clientupdate_1.0-1/usr/local/bin/hddsetpasswd.sh
+++ i386/clientupdate_1.0-1/usr/local/bin/hddsetpasswd.sh
@@ -8,7 +8,7 @@ if [ "$UNFROZEN" -eq "0" ]; then
     echo "Hard disk is frozen, the system will be set asleep, after wakeup it will resume setting the master password."
     echo "Press any key..."
     read
-    pmi action suspend || systemctl suspend
+    echo -n mem > /sys/power/state || pmi action suspend || systemctl suspend
 fi
 hdparm --user-master m --security-set-pass $2 $1
 
