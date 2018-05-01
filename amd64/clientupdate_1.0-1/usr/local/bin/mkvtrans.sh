2e2469d913c7dba471c2d912333d6f50957b45d1
JesusDF <jesusdf@gmail.com>
2018-05-01 21:14:31 +0200

    Try to use the ffmpeg binary included when mpv is built.

--- amd64/clientupdate_1.0-1/usr/local/bin/mkvtrans.sh
+++ amd64/clientupdate_1.0-1/usr/local/bin/mkvtrans.sh
@@ -5,12 +5,18 @@ FILE=$(basename "$PARAMS")
 SUBS=mysubs$RANDOM.${PARAMS##*.}
 THREADS=$(getconf _NPROCESSORS_ONLN)
 
+if [ -f /usr/src/mpv-build/ffmpeg_build/ffmpeg ]; then
+    FFMPEG=/usr/src/mpv-build/ffmpeg_build/ffmpeg
+else
+    FFMPEG=ffmpeg
+fi
+
 cd $DIR
 if [ -f $SUBS ]; then
     rm -f $SUBS
 fi
 ln -s "$FILE" $SUBS
-ffmpeg -threads $THREADS -i "$FILE" -vf "subtitles=$SUBS,scale=1280:720" -stats -c:v h264_nvenc -r ntsc -profile:v high -level 4.0 -rc cbr -acodec copy "${PARAMS%.*}.mp4" || ( rm -f "${PARAMS%.*}.mp4" && ffmpeg -threads $THREADS -i "$FILE" -vf subtitles=$SUBS -stats -c:v libx264 -r ntsc -profile:v high -preset faster -level 4.0 -rc cbr -acodec copy "${PARAMS%.*}.mp4" ) || ( rm -f "${PARAMS%.*}.mp4" && ffmpeg -threads $THREADS -i "$FILE" -stats -vcodec copy -acodec copy "${PARAMS%.*}.mp4" )
+${FFMPEG} -threads $THREADS -i "$FILE" -vf "subtitles=$SUBS,scale=1280:720" -stats -c:v h264_nvenc -r ntsc -profile:v high -level 4.0 -rc cbr -acodec copy "${PARAMS%.*}.mp4" || ( rm -f "${PARAMS%.*}.mp4" && ${FFMPEG} -threads $THREADS -i "$FILE" -vf subtitles=$SUBS -stats -c:v libx264 -r ntsc -profile:v high -preset faster -level 4.0 -rc cbr -acodec copy "${PARAMS%.*}.mp4" ) || ( rm -f "${PARAMS%.*}.mp4" && ${FFMPEG} -threads $THREADS -i "$FILE" -stats -vcodec copy -acodec copy "${PARAMS%.*}.mp4" )
 rm -f $SUBS
 
 DISPLAY=:0 /usr/bin/notify-send -i dialog-information -u normal "$FILE convertido a MP4.";
