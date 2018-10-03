e2b273a91062a567fc27a739b24fed749f83dbd9
JesusDF <jesusdf@gmail.com>
2018-10-03 22:39:57 +0200

    Fix a typo in the date format, also include a better name in the Phoronix Test Suite script.

--- amd64/clientupdate_1.0-1/usr/src/pts_run.sh
+++ amd64/clientupdate_1.0-1/usr/src/pts_run.sh
@@ -8,4 +8,4 @@
 
 # phoronix-test-suite install pts/ffmpeg pts/stream pts/unpack-linux
 phoronix-test-suite system-info
-TEST_RESULTS_NAME="$(dmidecode -t 2 | grep Tag | cut -d: -f2 | xargs) $(date +%Y-%M-%d_%H.%M.%S)" TEST_RESULTS_IDENTIFIER="PTS" TEST_RESULTS_DESCRIPTION="$(dmidecode -t 2 | grep Tag | cut -d: -f2 | xargs)" PRESET_OPTIONS="stream.run-type=Copy;" phoronix-test-suite benchmark pts/ffmpeg pts/stream pts/unpack-linux
+TEST_RESULTS_NAME="$(dmidecode -t 2 | grep Product | cut -d: -f2 | xargs) $(date +%Y-%m-%d_%H.%M.%S)" TEST_RESULTS_IDENTIFIER="$(dmidecode -t 2 | grep Product | cut -d: -f2 | xargs) $(dmidecode -t 2 | grep Tag | cut -d: -f2 | xargs)" TEST_RESULTS_DESCRIPTION="$(dmidecode -t 4 | grep Socket | cut -d: -f2 | xargs)" PRESET_OPTIONS="stream.run-type=Copy;" phoronix-test-suite benchmark pts/ffmpeg pts/stream pts/unpack-linux
