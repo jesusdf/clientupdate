e2135063801c4b3ae7904a2f13e9dc758e30a984
Jesús Diéguez Fernández <jesusdf@gmail.com>
2020-05-30 04:29:53 +0200

    Add missing software to the list.
    Signed-off-by: Jesús Diéguez Fernández <jesusdf@gmail.com>

--- amd64/clientupdate_1.0-1/etc/init.d/clientupdate.sh
+++ amd64/clientupdate_1.0-1/etc/init.d/clientupdate.sh
@@ -333,6 +333,10 @@ updatesystem)
         apt-get install -y seahorse;
         apt-get install -y ifupdown;
         apt-get install -y gufw;
+        apt-get install -y fprintd;
+        apt-get install -y libpam-fprintd;
+        apt-get install -y opensc-pkcs11;
+        apt-get install -y wakeonlan;
         apt-get install -y msmtp;
         apt-get install -y p7zip-full;
         apt-get install -y p7zip-rar;
@@ -413,6 +417,8 @@ updatesystem)
         apt-get install -y calibre;
         apt-get install -y blender;
         apt-get install -y gnome-system-monitor;
+        apt-get install -y mate-system-monitor;
+        apt-get install -y gnome-clocks;
         apt-get install -y ffmpeg;
         apt-get install -y youtube-dl;
         apt-get install -y youtube-dlg;
