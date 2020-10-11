c5cfbf6b30e394a33885abf28d30a9e7a3ce33f3
Jesús Diéguez Fernández <jesusdf@gmail.com>
2020-10-11 04:20:37 +0200

    Add new tbw.sh script to read the TeraBytes Written to an SSD.
    Signed-off-by: Jesús Diéguez Fernández <jesusdf@gmail.com>

---
+++ amd64/clientupdate_1.0-1/usr/local/bin/tbw.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+SECTOR_SIZE=$(smartctl -i /dev/$1 | grep "Sector Size" | xargs | cut -d\  -f3 ) 
+SECTORS_WRITTEN=$(smartctl -A /dev/$1 | grep LBAs | xargs | rev | cut -d\  -f1 | rev)
+
+if [ "$SECTORS_WRITTEN" == "" ]; then
+    echo "$1 is not an SSD."
+    exit 0
+fi
+
+MBW=$(( $SECTOR_SIZE * $SECTORS_WRITTEN / 1024 / 1024 ))
+GBW=$(( $MBW / 1024 ))
+TBW=$(( $GBW / 1024 ))
+
+if [ "$TBW" == "0" ]; then
+    if [ "$GBW" == "0" ]; then
+        echo "$MBW MBW"
+    else
+        echo "$GBW GBW / $MBW MBW"
+    fi
+else
+    echo "$TBW TBW / $GBW GBW"
+fi
