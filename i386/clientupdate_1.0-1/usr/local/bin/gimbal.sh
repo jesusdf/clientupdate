bd9625795384fb16a13c2eac5892e6bd6c05a41e
Jesús Diéguez Fernández <jesusdf@gmail.com>
2024-05-21 19:59:48 +0200

    Use MKV as the default container when stabilizing video.
    Signed-off-by: Jesús Diéguez Fernández <jesusdf@gmail.com>

--- i386/clientupdate_1.0-1/usr/local/bin/gimbal.sh
+++ i386/clientupdate_1.0-1/usr/local/bin/gimbal.sh
@@ -16,7 +16,7 @@ if [ -f $STAB ]; then
     rm -f $STAB
 fi
 ${FFMPEG} -threads $THREADS -i "$FILE" -vf "vidstabdetect=stepsize=6:shakiness=8:accuracy=9:result=$STAB" -f null -
-${FFMPEG} -threads $THREADS -i "$FILE" -vf "vidstabtransform=input=$STAB:zoom=1:smoothing=30,unsharp=5:5:0.8:3:3:0.4" -vcodec libx264 -preset slow -tune film -crf 18 -acodec copy "${PARAMS%.*}_stabilized.mp4"
+${FFMPEG} -threads $THREADS -i "$FILE" -vf "vidstabtransform=input=$STAB:zoom=1:smoothing=30,unsharp=5:5:0.8:3:3:0.4" -vcodec libx264 -preset slow -tune film -crf 18 -acodec copy "${PARAMS%.*}_stabilized.mkv"
 rm -f $STAB
 
 DISPLAY=:0 /usr/bin/notify-send -i dialog-information -u normal "$FILE estabilizado.";
