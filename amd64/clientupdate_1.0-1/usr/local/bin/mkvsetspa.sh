5c7470ac2528972c3f0f3a59fdfd27a3cc1a0e64
Jesús Diéguez Fernández <jesusdf@gmail.com>
2024-08-29 09:37:29 +0200

    Allow processing a single file or a complete folder.
    Signed-off-by: Jesús Diéguez Fernández <jesusdf@gmail.com>

--- amd64/clientupdate_1.0-1/usr/local/bin/mkvsetspa.sh
+++ amd64/clientupdate_1.0-1/usr/local/bin/mkvsetspa.sh
@@ -1,2 +1,26 @@
 #!/bin/bash
-find . -type f -name \*.mkv -exec mkvdefaultsub.sh ": es[-][eE][sS]|: es|: spa" "{}" \;
+PARAMS=$@
+
+if [[ -z $PARAMS ]]; then
+  echo "The file or folder parameter is missing, nothing to process."
+  echo "Usage examples:"
+  echo -e "\t$0 /my/folder"
+  echo -e "\t$0 file.mkv"
+  exit -1
+fi
+
+DIR=$(dirname "$PARAMS")
+FILE=$(basename "$PARAMS")
+LANG=": es[-][eE][sS]|: es|: spa"
+
+cd "$DIR"
+CURDIR=$(pwd)
+
+if [ -d "$FILE" ]; then
+  # Directory
+  find "$CURDIR" -type f -name \*.mkv -exec "$0" "{}" \;
+  exit 0
+fi
+
+# Single file
+mkvdefaultsub.sh "$LANG" "$FILE"
