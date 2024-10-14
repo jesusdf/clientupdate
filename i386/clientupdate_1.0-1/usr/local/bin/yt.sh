ed1c6356914d01e8bda75752de082b478d1fd84a
Jesús Diéguez Fernández <jesusdf@gmail.com>
2024-10-14 09:55:10 +0200

    Add referer parameter to the yt.sh script.
    Signed-off-by: Jesús Diéguez Fernández <jesusdf@gmail.com>

--- i386/clientupdate_1.0-1/usr/local/bin/yt.sh
+++ i386/clientupdate_1.0-1/usr/local/bin/yt.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-yt-dlp -f "bestvideo[ext!=webm][height <=? 1080]+bestaudio[ext!=webm]/best[ext!=webm]" --sub-lang es --write-sub -S res,vcodec:h264,acodec:aac,ext:mp4:m4a "$1"
+yt-dlp --referer "$2" -f "bestvideo[ext!=webm][height <=? 1080]+bestaudio[ext!=webm]/best[ext!=webm]" --sub-lang es --write-sub -S res,vcodec:h264,acodec:aac,ext:mp4:m4a "$1"
